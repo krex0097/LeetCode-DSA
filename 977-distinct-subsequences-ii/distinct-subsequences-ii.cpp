f1aeb175b89f7f28c8352cca773ddab28c2caee8
krex0097 <karan58er@gmail.com>
2026-09-08 15:03:49 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (31.94%) - LeetSync

--- 977-distinct-subsequences-ii/distinct-subsequences-ii.cpp
+++ 977-distinct-subsequences-ii/distinct-subsequences-ii.cpp
@@ -1,30 +1,18 @@
 class Solution {
 public:
-    using ll = long long;
-    const ll MOD= 1000000007;
-    vector<int> dp, prev, last;
-    ll f(int i) {
-        if (i < 0)
-            return 0;
-        if (dp[i] != -1)
-            return dp[i];
-        ll before = f(i - 1);
-        ll add = (before + 1) % MOD;
-        int p = prev[i];
-        if (p != -1) {
-            ll dup = f(p - 1);
-            add = (before - dup + MOD) % MOD;
-        }
-        return dp[i] = (before + add) % MOD;
-    }
+    const int MOD = 1e9 + 7;
     int distinctSubseqII(string s) {
         int n = s.size();
-        dp.assign(n, -1), prev.assign(n, -1), last.assign(26, -1);
-        for (int i = 0; i < n; i++) {
-            int c = s[i] - 'a';
-            prev[i] = last[c];
-            last[c] = i;
+        vector<int> last(26, -1);
+        vector<long long> dp(n + 1, 0);
+        for (int i = 1; i <= n; i++) {
+            int c = s[i - 1] - 'a';
+            dp[i] = (2 * dp[i - 1] + 1) % MOD;
+            if (last[c] != -1)
+                dp[i] = (2 * dp[i - 1] - dp[last[c] - 1] + MOD) % MOD;
+            last[c]=i;
         }
-        return f(n - 1);
+
+        return dp[n]%MOD;
     }
 };
