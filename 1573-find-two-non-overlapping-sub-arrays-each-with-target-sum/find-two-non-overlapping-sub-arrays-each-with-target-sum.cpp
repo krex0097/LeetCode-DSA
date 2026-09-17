bf61fe2bce356c5b87e8758e9993648944e20f91
krex0097 <karan58er@gmail.com>
2026-09-17 16:44:26 +0530

    Time: 4 ms (88.92%) | Memory: 88.9 MB (79.38%) - LeetSync

--- 1573-find-two-non-overlapping-sub-arrays-each-with-target-sum/find-two-non-overlapping-sub-arrays-each-with-target-sum.cpp
+++ 1573-find-two-non-overlapping-sub-arrays-each-with-target-sum/find-two-non-overlapping-sub-arrays-each-with-target-sum.cpp
@@ -7,17 +7,15 @@ public:
         int minLen = INT_MAX, minPair = INT_MAX, sum = 0;
         while (j < n) {
             sum += arr[j];
-            last[j]=minLen;
-            while (i <= j && sum >= target) {
-                if (sum == target) {
-                    int len = j - i + 1;
-                    minLen = min(minLen, len);
-                    last[j] = minLen;
-                    if (i > 0 && last[i - 1] != INT_MAX)
-                        minPair = min(minPair, last[i - 1] + len);
-                }
-                sum -= arr[i];
-                i++;
+            last[j] = minLen;
+            while (i <= j && sum > target)
+                sum -= arr[i], i++;
+            if (sum == target) {
+                int len = j - i + 1;
+                minLen = min(minLen, len);
+                last[j] = minLen;
+                if (i > 0 && last[i - 1] != INT_MAX)
+                    minPair = min(minPair, last[i - 1] + len);
             }
             j++;
         }
